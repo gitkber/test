8c96633ad63c44437dce7dfdbb83341f1f08c5e7
berth.k <test>
2015-04-30 13:16:11 +0200

    Initial revision

---
+++ FirstSpringMVC/src/be/firstspringmvc/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package be.firstspringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("welcome")
+	public ModelAndView helloworld() {
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from HelloWorldController.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+
+}
